a39d01570284bb229a206b3c55642665dc65839a
Yusuke Yasui <ysk.univ.1007@gmail.com>
2015-02-21 17:29:34 +0900

    tekito-

---
+++ init.el
@@ -0,0 +1,24 @@
+;; -*- lexical-binding: t -*-
+
+(defun eval-when-frame-is-created (f)
+  (funcall f)
+  (add-hook 'after-make-frame-functions
+	    '(lambda (frame)
+	       (with-selected-frame frame
+		 (funcall f)))))
+
+;; C-h to Backspace
+(eval-when-frame-is-created (keyboard-translate ?\C-h ?\C-?))
+
+;;; erase evil bars
+(menu-bar-mode -1)
+(if (window-system)
+    (eval-when-frame-is-created
+     (lambda ()
+       (tool-bar-mode -1)
+       (toggle-scroll-bar -1))))
+
+;; font color
+(add-to-list 'default-frame-alist '(cursor-color . "gray"))
+(add-to-list 'default-frame-alist '(foreground-color . "gray"))
+(add-to-list 'default-frame-alist '(background-color . "black"))
