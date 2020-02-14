de9035aa8c7eca964acd413003cd74cadd00c79a
ysk_univ <ysk.univ.1007@gmail.com>
2020-02-14 23:50:09 +0900

    enable el-get sync

--- init.el
+++ init.el
@@ -20,7 +20,7 @@
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 (setq el-get-user-package-directory "~/.emacs.d/el-get-init-files/")
 
-;; (el-get 'sync)
+(el-get 'sync)
 
 (setq dired-bind-jump nil)		; for ddskk
 (el-get-bundle helm)
