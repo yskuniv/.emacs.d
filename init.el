6da8d5116a7283c06d6dbb47b421c9125bac146a
ysk_univ <ysk.univ.1007@gmail.com>
2021-03-12 21:09:43 +0900

    fix position of el-get sync

--- init.el
+++ init.el
@@ -20,14 +20,14 @@
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 (setq el-get-user-package-directory "~/.emacs.d/el-get-init-files/")
 
-(el-get 'sync)
-
 ;; (setq dired-bind-jump nil)		; for ddskk
 (el-get-bundle helm)
 (el-get-bundle auto-complete)
 ;; (el-get-bundle ddskk)
 (el-get-bundle paredit)
 (el-get-bundle magit)
+
+(el-get 'sync)
 
 
 ;;; key bindings
